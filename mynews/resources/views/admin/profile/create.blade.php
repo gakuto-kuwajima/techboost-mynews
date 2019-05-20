5eab8099c1040308be105b1cc5ac6fb2f66522f9
gakuto <mydance365@gmail.com>
2019-05-20 17:21:20 +0900

    2019-05-20 windows

--- mynews/resources/views/admin/profile/create.blade.php
+++ mynews/resources/views/admin/profile/create.blade.php
@@ -11,6 +11,43 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>Myプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">性別</label>
+                        <div class="col-md-10">
+                            <input type="radio" name="gendar" value="male">男性
+                            <input type="radio" name="gendar" value="female">女性
+                            <input type="radio" name="gendar" value="noanswer">その他
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hody" rows="5">{{ old('body')}}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="intoduction" rows="15">{{ old('body')}}</textarea>
+                        </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
