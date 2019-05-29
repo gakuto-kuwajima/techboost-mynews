21262cd67307ce220cd368b5b5f428fe60c703ba
gakuto <mydance365@gmail.com>
2019-05-29 17:38:37 +0900

    2019-05-29 windows

--- mynews/app/Http/Controllers/Admin/NewsController.php
+++ mynews/app/Http/Controllers/Admin/NewsController.php
@@ -51,10 +51,53 @@ class NewsController extends Controller
         $cond_title = $request->cond_title;
         if ($cond_title != ''){
             // 検索されたら検索結果を取得する
-            $post = News::where('title', $cond_title)->get();
+            $posts = News::where('title', $cond_title)->get();
         } else {
             $posts = News::all();
         }
-        return view('admin.news.index', ['post' => $post, 'cond_title' => $cond_title]);
+        return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+
+    public function edit(Request $request)
+    {
+        // News Modelからデータを取得する
+        $news = News::find($request->id);
+        if (empty($news)) {
+          abort(404);
+        }
+        return view('admin.news.edit', ['news_form' => $news]);
+    }
+
+    public function update(Request $request)
+    {
+        // Validationをかける
+        $this->validate($request, News::$rules);
+        // News Modelからデータを取得する
+        $news = News::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $news_form = $request->all();
+        if (isset($news_form['image'])){
+          $path = $request->file('image')->store('public/image');
+          $news->image_path = basename($path);
+          unset($news_form['image']);
+        } elseif (0 == strcmp($request->remove, 'true')){
+          $news->image_path = null;
+        }
+        unset($news_form['_token']);
+        unset($news_form['remove']);
+
+        // 該当するデータを上書きして保存する
+        $news->fill($news_form)->save();
+
+        return redirect('admin/news');
+    }
+
+    public function delete(Request $request)
+    {
+        // 該当するNews Modelを取得
+        $news = News::find($request->id);
+        // 削除する
+        $news->delete();
+        return redirect('admin/news/');
     }
   }
